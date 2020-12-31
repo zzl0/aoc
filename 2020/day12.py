e9a36defceed5f6f80fe9f8e0c3deed482fb6cce
zzl0 <zhuzhaolong0@gmail.com>
2020-12-31 12:28:02 -0500

    day 12-2

--- 2020/day12.py
+++ 2020/day12.py
@@ -1,63 +1,58 @@
 from utils import *
 
+DIRECTIONS = dict(E=(1, 0), W=(-1, 0), N=(0, 1), S=(0, -1))
+ROTATE_FACTOR = dict(R=(1, -1), L=(-1, 1))
+
 
 def parse_instruction(s):
-    action = s[0]
-    return action, int(s[1:])
+    return s[0], int(s[1:])
 
 
+# This is a refactored version based on Peter Norvig's solution.
 class Ship:
-    def __init__(self, direction='E', x=0, y=0):
+    def __init__(self, pos=(0, 0), direction=DIRECTIONS['E']):
         self.direction = direction
-        self.x = x # E/W
-        self.y = y # N/S
-        self.flag = {'E': 1, 'W': -1, 'N': 1, 'S': -1}
+        self.pos = pos
 
-    def run_instruction(self, instruction):
-        action, val = instruction
-        if action == 'F':
-            action = self.direction
+    def run(self, instructions, waypoint=False):
+        for action, val in instructions:
+            if action in 'LR':
+                self.direction = self.rotate(action, val, *self.direction)
+            elif action == 'F':
+                self.pos = self.go(val, *self.pos, *self.direction)
+            else:
+                field = 'direction'if waypoint else 'pos'
+                setattr(self, field, self.go(val, *getattr(self, field), *DIRECTIONS[action]))
 
-        if action in 'EW':
-            self.x += val * self.flag[action]
-        elif action in 'NS':
-            self.y += val * self.flag[action]
-        else:
-            self.direction = Ship.next_direction(self.direction, instruction)
+    def rotate(self, action, degrees, x, y):
+        dx, dy = ROTATE_FACTOR[action]
+        for _ in range(degrees // 90):
+            x, y = y * dx, x * dy
+        return x, y
+
+    def go(self, val, x, y, dx, dy):
+        return x + dx * val, y + dy * val
 
     def manhattan_distance(self):
-        return abs(self.x) + abs(self.y)
-
-    @staticmethod
-    def next_direction(direction, instruction):
-        action, val = instruction
-        if not action in 'LR':
-            return direction
-        directions = 'ESWN'
-        offset = val // 90 * (1 if action == 'R' else -1)
-        idx = directions.index(direction)
-        return directions[(idx + offset) % len(directions)]
-
-
-def test():
-    next_direction = Ship.next_direction
-    assert next_direction('W', ('R', 90)) == 'N'
-    assert next_direction('W', ('R', 180)) == 'E'
-    assert next_direction('W', ('R', 270)) == 'S'
-    assert next_direction('W', ('L', 90)) == 'S'
-    assert next_direction('W', ('L', 180)) == 'E'
-    assert next_direction('W', ('L', 270)) == 'N'
-    print('test passes')
-test()
+        return sum(map(abs, self.pos))
 
 
 def day12_1(instructions):
-    pos = Ship()
-    for instruction in instructions:
-        pos.run_instruction(instruction)
-    return pos.manhattan_distance()
+    ship = Ship()
+    ship.run(instructions)
+    return ship.manhattan_distance()
+
+
+def day12_2(instructions):
+    ship = Ship(direction=(10, 1))
+    ship.run(instructions, True)
+    return ship.manhattan_distance()
 
 
 if __name__ == "__main__":
     instructions = data(12, parse_instruction)
     print(f"day12_1: {day12_1(instructions)}")
+    print(f"day12_2: {day12_2(instructions)}")
+    # day12_1: 1603
+    # day12_2: 52866
+    # python3 day12.py  0.03s user 0.01s system 88% cpu 0.049 total
