f59d12c8d84387deb05c440e5d8a72a334a8d39c
zzl0 <zhuzhaolong0@gmail.com>
2022-10-29 17:13:37 -0400

    2020 rust 6 part1

---
+++ 2020/rust/src/bin/06.rs
@@ -0,0 +1,34 @@
+use im::HashSet;
+use std::fmt;
+
+pub struct Answers(HashSet<u8>);
+
+impl fmt::Debug for Answers {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for &answer in &self.0 {
+            write!(f, "{}", answer as char)?;
+        }
+        Ok(())
+    }
+}
+
+fn part1(input: &str) {
+    let answer: usize = input
+        .split("\n\n")
+        .map(|group| {
+            HashSet::<u8>::unions(
+                group
+                    .lines()
+                    .map(|line| line.as_bytes().iter().copied().collect()),
+            )
+            .len()
+        })
+        .sum();
+
+    println!("part1: {}", answer);
+}
+
+fn main() {
+    let input = include_str!("../../../data/day06.txt");
+    part1(input);
+}
