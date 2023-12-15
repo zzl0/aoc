d8c32de75c41284c2c681f5ecc8e833d6e56dae7
Zhaolong Zhu <zhaolong@meta.com>
2023-12-14 23:47:29 -0500

    2023 day10

---
+++ 2023/rust/src/bin/day10.rs
@@ -0,0 +1,96 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+fn connections(tile: u8) -> [bool; 4] {
+    match tile {
+        //      [ up, right,  down,  left]
+        b'|' => [true, false, true, false],
+        b'-' => [false, true, false, true],
+        b'L' => [true, true, false, false],
+        b'J' => [true, false, false, true],
+        b'7' => [false, false, true, true],
+        b'F' => [false, true, true, false],
+        _ => [false, false, false, false],
+    }
+}
+
+fn find_loop(graph: &[Vec<[bool; 4]>], start: (usize, usize)) -> Option<HashSet<(usize, usize)>> {
+    let (mut r, mut c) = start;
+    let mut d = graph[r][c].iter().position(|&d| d).unwrap();
+    let mut seen = HashSet::new();
+    loop {
+        if !seen.insert((r, c)) {
+            break Some(seen);
+        }
+
+        let came_from = match d {
+            0 => {
+                r -= 1;
+                2
+            }
+            1 => {
+                c += 1;
+                3
+            }
+            2 => {
+                r += 1;
+                0
+            }
+            3 => {
+                c -= 1;
+                1
+            }
+            _ => unreachable!(),
+        };
+
+        if !graph[r][c][came_from] {
+            break None;
+        }
+
+        d = (0..4).find(|&i| i != came_from && graph[r][c][i]).unwrap();
+    }
+}
+
+fn main() {
+    let input = include_str!("../../input/day10.txt");
+    let mut start = (0, 0);
+    let mut graph = input
+        .split('\n')
+        .enumerate()
+        .map(|(r, line)| {
+            line.bytes()
+                .enumerate()
+                .map(|(c, tile)| {
+                    if tile == b'S' {
+                        start = (r, c);
+                    }
+                    connections(tile)
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+    let pipe_loop = "J|-L7F"
+        .bytes()
+        .find_map(|start_tile| {
+            graph[start.0][start.1] = connections(start_tile);
+            find_loop(&graph, start)
+        })
+        .unwrap();
+
+    let p1 = pipe_loop.len() / 2;
+    println!("part1: {}", p1);
+
+    let mut p2 = 0;
+    for r in 0..graph.len() {
+        let mut inside = false;
+        for c in 0..graph[0].len() {
+            if !pipe_loop.contains(&(r, c)) {
+                p2 += inside as usize;
+            } else if graph[r][c][0] {
+                inside = !inside;
+            }
+        }
+    }
+    println!("part2: {}", p2);
+}
